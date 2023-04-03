7ddb4e33f8da0862797b29a7b93ab8b05aec0bd1
DerpyTheSpy <erickim334@gmail.com>
2023-04-03 15:48:24 -0400

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MissionlistComponent } from './missionlist/missionlist.component';
+import { MissionfilterComponent } from './missionfilter/missionfilter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MissionlistComponent,
+    MissionfilterComponent
   ],
   imports: [
     BrowserModule,
